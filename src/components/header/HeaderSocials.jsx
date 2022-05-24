a6318cf039d853be3dca66edfd87a5f011fac662
Albert <ialbertbislimi@gmail.com>
2022-05-24 19:52:59 +0200

    init

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
-import {FiDribbble} from 'react-icons/fi'
+import {BsTwitter} from 'react-icons/bs'
 
 
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://linkedin.com" target="_blank"><BsLinkedin /></a>
-        <a href="https://github.com" target="_blank"><FaGithub/></a>
-        <a href="https://dribble.com" target="_blank"><FiDribbble/></a>
+        <a href="https://www.linkedin.com/in/albert-bislimi/" target="_blank"><BsLinkedin /></a>
+        <a href="https://github.com/AlbertBislimi" target="_blank"><FaGithub/></a>
+        <a href="https://twitter.com/AlbertBislimi" target="_blank"><BsTwitter/></a>
     </div>
   )
 }
