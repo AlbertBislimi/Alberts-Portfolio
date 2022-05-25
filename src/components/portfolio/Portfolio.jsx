e9d0c2755ebfe29646858f8390dcd8a960165a69
Albert <ialbertbislimi@gmail.com>
2022-05-25 19:57:38 +0200

    small changes

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,6 @@ import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.jpg'
 import IMG2 from '../../assets/portfolio2.png'
 import IMG3 from '../../assets/portfolio3.jpg'
-import IMG4 from '../../assets/portfolio4.jpg'
 
 const data = [
   {
