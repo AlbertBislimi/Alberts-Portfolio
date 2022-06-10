e1b633f101387df0a13e3c6628d005716f67596d
Albert <ialbertbislimi@gmail.com>
2022-06-10 18:01:23 +0200

    fixed whatsapp number

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -41,7 +41,7 @@ const Contact = () => {
             <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
             <h5>+123456789</h5>
-            <a href="https://api.whatsapp.com/send?phone+38349588569" target="_blank">Send a message</a>
+            <a href="https://api.whatsapp.com/send?phone=38349588569" target="_blank">Send a message</a>
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
