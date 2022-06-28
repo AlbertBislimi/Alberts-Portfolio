d684a8da70ca10d0e27f48cc9b7022f92d096a15
Albert <ialbertbislimi@gmail.com>
2022-06-28 16:13:22 +0200

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,12 +10,12 @@ const Footer = () => {
       <a href="#" className='footer__logo'>Albert</a>
 
       <ul className='permalinks'>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Experience</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Portfolio</a></li>
-        <li><a href="#">Testimonials</a></li>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portofolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
       </ul>
 
       <div className="footer__socials">
