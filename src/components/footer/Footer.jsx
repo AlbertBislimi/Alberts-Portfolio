e2764cafc00f31c346000b4d1f2dfb0dc2525443
Albert <ialbertbislimi@gmail.com>
2022-07-07 09:40:11 +0200

    updated linkedin icon

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './footer.css'
 import {BsGithub} from 'react-icons/bs'
-import {FiInstagram} from 'react-icons/fi'
+import {FaLinkedin} from 'react-icons/fa'
 import {BsTwitter} from 'react-icons/bs'
 
 const Footer = () => {
@@ -10,16 +10,16 @@ const Footer = () => {
       <a href="#" className='footer__logo'>Albert</a>
 
       <ul className='permalinks'>
-        <li><a href="#header">Home</a></li>
+        <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
-        <li><a href="#services">Services</a></li>
+        {/*<li><a href="#services">Services</a></li>*/}
         <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#testimonials">Testimonials</a></li>
       </ul>
 
       <div className="footer__socials">
         <a href="https://github.com/AlbertBislimi"><BsGithub/></a>
-        <a href="https://instagram.com/ialbertbislimi"><FiInstagram/></a>
+        <a href="https://www.linkedin.com/in/albert-bislimi/"><FaLinkedin/></a>
         <a href="https://twitter.com/AlbertBislimi"><BsTwitter/></a>
       </div>
 
